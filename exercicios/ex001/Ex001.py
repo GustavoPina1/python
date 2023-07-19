a21d007aba627a388efaff8f0799ec05578e2ce5
Gustavo <gustavoqtp@gmail.com>
2023-07-19 16:12:48 -0300

    Alteração

---
+++ exercicios/ex001/Ex001.py
@@ -0,0 +1,16 @@
+print("Olá, Mundo")
+
+print("Olá "+str(5)) #casting
+print("Olá",5)
+
+nome="Gustavo"
+idade=25
+altura=1.88
+
+print(nome,idade,altura)
+
+name=input("Digite o seu nome: ")
+age=int(input("Qual sua idade: "))
+height=float(input("Qual sua altura: "))
+
+print(name,age,height)
