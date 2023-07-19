286092cea0e30008190c1b7b7f61180dd51818d1
Gustavo <gustavoqtp@gmail.com>
2023-07-19 16:29:38 -0300

    Alteração de nomes das pastas

---
+++ desafios/desafio001/desafio001.py
@@ -0,0 +1,6 @@
+'''Desafio 1: Crie um script pyhton que leia o nome de uma pessoa
+e mostre uma mensagem de boas vindas para ela'''
+
+nome=input("Qual o seu nome: ")
+print("Seja bem vindo",nome)
+
