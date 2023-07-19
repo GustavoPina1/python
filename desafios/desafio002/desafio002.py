39b93bd515315d0f138da27ada4713de3aadc102
Gustavo <gustavoqtp@gmail.com>
2023-07-19 16:48:42 -0300

    Alteração desafio 002

---
+++ desafios/desafio002/desafio002.py
@@ -0,0 +1,8 @@
+'''Desafio 2: Crie um script python que leia o dia, mês e ano
+de nascimento de uma pessoa e mostre a data formatada'''
+
+dia = int(input("Dia: "))
+mes= int(input("Mes: "))
+ano=int(input("Ano: "))
+
+print("Você nasceu no dia "+ str(dia)+"/"+str(mes)+"/"+str(ano)+", correto?")
---
+++ desafios/desafio003/desafio003.py
@@ -0,0 +1,6 @@
+'''Desafio 3: Crie um programa que leia 2 números e mostre a soma deles'''
+
+num1=float(input("Numero 1: "))
+num2=float(input("Numero 2: "))
+
+print("Soma:",num1+num2)
